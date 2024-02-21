f67621dc22df906533c7155d3ca7874dfd089154
Boon <b40248b@gmail.com>
2024-02-21 13:19:34 +0700

    try by p'note

--- techtech.js
+++ techtech.js
@@ -1,15 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/addons/p5.sound.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8" />
 
-  </head>
-  <body>
-    <main>
-    </main>
-    <script src="sketch.js"></script>
-  </body>
-</html>
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+  }
+  
+  function draw() {
+    background(250);
+    
+    rect(50,90,235,260)
+    
+    push()
+    textFont('Gotham')
+    textAlign(CENTER)
+    textSize(40)
+    text("PHUWIT VITAYARUEANGDEJ",windowWidth/2,50)
+    pop()
+    
+    
+    textFont('Gotham')
+    textSize(20)
+    text("Personal information",320,110)
+    
+    textSize(14)
+    text("Contact: +6699-336-0161 ",320,135)
+    text("E-mail:    b40248b@gmail.com ",320,155)
+    text("Address: The Nest Chula - Samyan,",320,175)
+    text("Bang Rak District, Bangkok ,",388,195)
+    text("10500,",388,215)
+    
+    textSize(20)
+    text("Education",320,255)
+    
+    textSize(14)
+    text("Bachelor of Industrial Design (B.I.D.)",320,280)
+    text("Faculty of Architecture",320,300)
+    text("Chulalongkorn University",320,320)
+    text("2021 - 2024,",320,340)
+    
+  
+    textSize(20)
+    text("Interest",320,395)
+    
+    textSize(14)
+    text("Tech coding",320,420)
+    text("Plastic design",320,440)
+    text("Ceramic and Form design",320,460)
+    text("3D printing",320,480)
+    text("Experience design",320,500)
+    text("Style and temporary design",320,520)
+    text("Coffee",320,540)
+    text("Music",320,560)
+    text("Post Malone",320,580)
+    text("Morning Glory",320,600)
+    
+    textSize(20)
+    text("Softwares",50,395)
+    
+    textSize(14)
+    text("Rhinoceros 7",50,420)
+    text("Enscape",50,440)
+    text("Keyshot 11",50,460)
+    text("Adobe Photoshop",50,480)
+    text("Figma ",50,500)
+    text("Adobe Illustrator",50,520)
+    text("Adobe Lightroom",50,540)
+    text("Adobe Premier Pro",50,560)
+    text("Procreate",50,580)
+    text("Vllo",50,600)
+  
+    
+  }
